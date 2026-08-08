7ea273100d8019a37ede0697f03032e68b5b3571
Diego <diegofelipe1025@gmail.com>
2026-08-08 18:08:54 -0300

    feat:initializing the back and front end

---
+++ back_end/src/test/java/bico_digital/BicoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package bico_digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
